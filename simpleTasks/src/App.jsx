81d09b68865ee7e2080d77bc647baa2737a51100
Raman Prit <rp231249@gmail.com>
2026-09-21 11:34:14 +0530

    task2

--- simpleTasks/src/App.jsx
+++ simpleTasks/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Task1 from './components/Task1'
+import Task2 from './components/Task2'
 
 export default function App() {
   return (
     <div>
-      <Task1 />
+      {/* <Task1 /> */}
+      <Task2 />
     </div>
   )
 }
